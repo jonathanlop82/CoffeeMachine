27dd4c97a06b5c24a2b93b2ff10c0b14b727e778
Tecnologia <jonathanlop82@gmail.com>
2021-12-12 18:20:06 -0500

    Done the project

--- main.py
+++ main.py
@@ -30,19 +30,37 @@ resources = {
     "coffee": 100,
 }
 
-def get_resourses(selection, drink_to_dispense):
+def check_resourses(selection, drink_to_dispense):
     # Water
     ingredients = drink_to_dispense["ingredients"]
     #print(ingredients)
     if ingredients["water"] <= resources["water"]:
-        print("Ok")
+        return True
+    else:
+        print("Sorry there is not enough water.")
+        return False
     if not(selection == "espresso"):
         if ingredients["milk"] <= resources["milk"]:
-            print("Ok")
+            return True
+        else:
+            print("Sorry there is not enough milk.")
+            return False
     if ingredients["coffee"] <= resources["coffee"]:
-        print("Ok")
+        return True
+    else:
+        print("Sorry there is not enough coffee.")
+        return False
+
+def get_resourses(selection, drink_to_dispense):
+    ingredients = drink_to_dispense["ingredients"]
+    resources["water"] = resources["water"] - ingredients["water"]
+    if not (selection == "espresso"):
+        resources["milk"] = resources["milk"] - ingredients["milk"]
+    resources["coffee"] = resources["coffee"] - ingredients["coffee"]
+
 
 def process_coins(selection,drink_to_dispense):
+    global money
     print("Please insert coins.")
     quarters = float(input("how many quarters?: "))
     dimes = float(input("how many dimes?: "))
@@ -55,21 +73,30 @@ def process_coins(selection,drink_to_dispense):
         get_resourses(selection, drink_to_dispense)
         print(f"Here is ${round((total_coins - drink_to_dispense['cost']),2)} in change.")
         print(f"Here is your {selection} ☕️. Enjoy!")
+        money += drink_to_dispense['cost']
     else:
         print("Sorry that's not enough money. Money refunded.")
 
-selection = input("What would you like? (espresso/latte/cappuccino): ")
 money = 0
+machine_on = True
 
-if selection == "espresso" or selection == "latte" or selection == "cappuccino":
-    drink_to_dispense = MENU[selection]
-    print(drink_to_dispense)
-    process_coins(selection, drink_to_dispense)
-elif selection == "report":
-    for item in resources:
-        print(f"{item.capitalize()}: {resources[item]}")
-    print(f"Money: ${money}")
-else:
-    print("Wrong selection")
+while machine_on:
+
+    selection = input("What would you like? (espresso/latte/cappuccino): ")
+
+
+    if selection == "espresso" or selection == "latte" or selection == "cappuccino":
+        drink_to_dispense = MENU[selection]
+        print(drink_to_dispense)
+        if check_resourses(selection, drink_to_dispense):
+            process_coins(selection, drink_to_dispense)
+    elif selection == "report":
+        for item in resources:
+            print(f"{item.capitalize()}: {resources[item]}")
+        print(f"Money: ${money}")
+    elif selection == "off":
+        machine_on = False
+    else:
+        print("Wrong selection")
 
 
